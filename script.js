aca54516bec2923e4f6b3489cc5bfb9e94140596
Isabellla-cyber <83213348+Isabellla-cyber@users.noreply.github.com>
2021-09-01 21:39:50 -0700

    Update script.js

--- script.js
+++ script.js
@@ -85,7 +85,7 @@ $("document").ready(function () {
 
 function sendEmailDefault() {
   var email = "isabellabela41@hotmail.com";
-  var subject = "Test Email";
+  var subject = "Email";
   var msgBody = "Thank you for contacting us, we will get back to you shortly!";
   window.open(`mailto:${email}?subject=${subject}&body=${msgBody}`);
 }
